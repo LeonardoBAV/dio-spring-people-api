4d651c01ede18e36f807d23ab7bdbb6752466fa2
Leonardo Vasconcelos <leonardo.vasconcelos@apis3.com>
2021-10-18 21:48:54 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/diospringpeopleapi/DioSpringPeopleApiApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.diospringpeopleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioSpringPeopleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioSpringPeopleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/diospringpeopleapi/DioSpringPeopleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.diospringpeopleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioSpringPeopleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
